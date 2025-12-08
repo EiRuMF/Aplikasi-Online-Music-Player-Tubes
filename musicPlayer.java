684dfe8830fd6fc13b20bbb5e93391df9b897c4c
EiRuMF <iuriemf@gmail.com>
2025-12-08 16:48:08 +0700

    Permulaan

---
+++ musicPlayer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner
+
+public class MusicPlayer{
+  
+  static void String[] 
+
+  public static void main(string [] args){
+      Scanner in = new Scanner(System.in);
+
+      
+
+      
+
+
+  }
+}
